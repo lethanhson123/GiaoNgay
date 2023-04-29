3472d7becc403e6d630a34a69f52b87611381369
lethanhson1234 <tinhnghesohu@gmail.com>
2023-04-29 17:38:22 +0700

    20230429

---
+++ API/Controllers/v1/SOHUController.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Controllers.v1
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class SOHUController : BaseController<Bank, IBankRepository>
+    {
+        private readonly IBankRepository _bankRepository;
+        public SOHUController(IBankRepository bankRepository) : base(bankRepository)
+        {
+            _bankRepository = bankRepository;
+        }
+    }
+}
